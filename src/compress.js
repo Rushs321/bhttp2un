192ca089c240f442f755045d622eb7a2b09b41b8
Yonle <yonle@duck.com>
2022-07-04 21:59:01 +0700

    Better Checking, Error handling, and redirecting - Better checking. - Fix Vulnerability where a small container   was requested to download 1 GB of ISO which cause   full memory usage, Especially loads. - Pipe the incomming buffer to client when it's not a photo - Say hi to miniget
    Signed-off-by: Yonle <yonle@duck.com>

--- src/compress.js
+++ src/compress.js
@@ -1,27 +1,42 @@
+/*
+ * compress.js
+ * A module that compress a image.
+ * compress(httpRequest, httpResponse, ReadableStream);
+ */
 const sharp = require('sharp')
 const redirect = require('./redirect')
 
 function compress(req, res, input) {
   const format = req.params.webp ? 'webp' : 'jpeg'
 
-  sharp(input)
-    .grayscale(req.params.grayscale)
-    .toFormat(format, {
-      quality: req.params.quality,
-      progressive: true,
-      optimizeScans: true
-    })
-    .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) return redirect(req, res)
+  /*
+   * input.pipe => sharp (The compressor) => Send to httpResponse
+   * The following headers:   
+   * |  Header Name  |            Description            |           Value            |
+   * |---------------|-----------------------------------|----------------------------|
+   * |x-original-size|Original photo size                |OriginSize                  |
+   * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size |
+   */
+  input.pipe(
+    sharp()
+      .grayscale(req.params.grayscale)
+      .toFormat(format, {
+        quality: req.params.quality,
+        progressive: true,
+        optimizeScans: true
+      })
+      .toBuffer((err, output, info) => {
+        if (err || !info || res.headersSent) return redirect(req, res)
 
-      res.setHeader('content-type', `image/${format}`)
-      res.setHeader('content-length', info.size)
-      res.setHeader('x-original-size', req.params.originSize)
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size)
-      res.status(200)
-      res.write(output)
-      res.end()
-    })
+        res.setHeader('content-type', `image/${format}`)
+        res.setHeader('content-length', info.size)
+        res.setHeader('x-original-size', req.params.originSize)
+        res.setHeader('x-bytes-saved', req.params.originSize - info.size)
+        res.status(200)
+        res.write(output);
+        res.end();
+      })
+    );
 }
 
 module.exports = compress
--- src/proxy.js
+++ src/proxy.js
@@ -1,43 +1,70 @@
-const request = require('request')
-const pick = require('lodash').pick
-const shouldCompress = require('./shouldCompress')
-const redirect = require('./redirect')
-const compress = require('./compress')
-const bypass = require('./bypass')
-const copyHeaders = require('./copyHeaders')
+/*
+ * proxy.js
+ * The bandwidth hero proxy handler.
+ * proxy(httpRequest, httpResponse);
+ */
+const get = require("miniget");
+const pick = require("lodash").pick;
+const shouldCompress = require("./shouldCompress");
+const redirect = require("./redirect");
+const compress = require("./compress");
+const copyHeaders = require("./copyHeaders");
 
 function proxy(req, res) {
-  request.get(
-    req.params.url,
-    {
-      headers: {
-        ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-        'user-agent': 'Bandwidth-Hero Compressor',
-        'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-        via: '1.1 bandwidth-hero'
-      },
-      timeout: 10000,
-      maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
+  let origin = get(req.params.url, {
+    headers: {
+      ...pick(req.headers, ["cookie", "dnt", "referer"]),
+      "user-agent": "Bandwidth-Hero Compressor",
+      "x-forwarded-for": req.headers["x-forwarded-for"] || req.ip,
+      via: "1.1 bandwidth-hero",
     },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) return redirect(req, res)
+    timeout: 10000,
+    maxRedirects: 5,
+    encoding: null,
+    strictSSL: false,
+    gzip: true,
+    jar: true,
+  });
 
-      copyHeaders(origin, res)
-      res.setHeader('content-encoding', 'identity')
-      req.params.originType = origin.headers['content-type'] || ''
-      req.params.originSize = buffer.length
+  /*
+   * When there's a error, Redirect then destroy the stream immediately.
+   */
 
-      if (shouldCompress(req)) {
-        compress(req, res, buffer)
-      } else {
-        bypass(req, res, buffer)
-      }
+  origin.on("error", () => {
+    redirect(req, res);
+    return origin.destroy();
+  });
+
+  origin.on("response", (response) => {
+    if (res.statusCode >= 400) {
+      redirect(req, res);
+      return origin.destroy();
     }
-  )
+
+    copyHeaders(response, res);
+    res.setHeader("content-encoding", "identity");
+    req.params.originType = response.headers["content-type"] || "";
+    req.params.originSize = response.headers["content-length"] || "0";
+
+    if (shouldCompress(req)) {
+      /*
+       * sharp support stream. So pipe it.
+       */
+      return compress(req, res, origin);
+    } else {
+      /*
+       * Downloading then uploading the buffer to the client is not a good idea though
+       * It would better if you pipe it to client instantly.
+       */
+
+      res.setHeader("x-proxy-bypass", 1);
+      res.setHeader(
+        "content-length",
+        response.headers["content-length"] || "0"
+      );
+      return origin.pipe(res);
+    }
+  });
 }
 
-module.exports = proxy
+module.exports = proxy;
