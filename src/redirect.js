30473a6fbda7a9d7185713ed169e27d729ede6d9
Yonle <yonle@lecturify.net>
2023-06-03 12:43:26 +0700

    log about redirect too
    Signed-off-by: Yonle <yonle@lecturify.net>

--- src/redirect.js
+++ src/redirect.js
@@ -1,13 +1,15 @@
 function redirect(req, res) {
-  if (res.headersSent) return
+  if (res.headersSent) return;
 
-  res.setHeader('content-length', 0)
-  res.removeHeader('cache-control')
-  res.removeHeader('expires')
-  res.removeHeader('date')
-  res.removeHeader('etag')
-  res.setHeader('location', encodeURI(req.params.url))
-  res.status(302).end()
+  res.setHeader('content-length', 0);
+  res.removeHeader('cache-control');
+  res.removeHeader('expires');
+  res.removeHeader('date');
+  res.removeHeader('etag');
+  res.setHeader('location', encodeURI(req.params.url));
+  res.status(302).end();
+
+  console.log(`Worker ${process.pid}:`, `Redirected to ${req.params.url}`);
 }
 
 module.exports = redirect
