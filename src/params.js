64c47ea67f9c7ae511219ce3043e761e46065168
Yonle <yonle@lecturify.net>
2024-01-27 22:48:26 +0700

    fix bandiwidth-hero-proxy did not shows up
    Signed-off-by: Yonle <yonle@lecturify.net>

--- src/params.js
+++ src/params.js
@@ -2,10 +2,10 @@
 const DEFAULT_QUALITY = 40
 
 function params(req, res, next) {
-  let url = decodeURIComponent(req.query.url);
-  if (!url) return res.end('bandwidth-hero-proxy')
+  let url = req.query.url;
+  if (!url) return res.send('bandwidth-hero-proxy');
 
-  req.params.url = url
+  req.params.url = decodeURIComponent(url);
   req.params.webp = !req.query.jpeg
   req.params.grayscale = req.query.bw != 0
   req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
