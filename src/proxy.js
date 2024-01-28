6062205131d9621f831a22c2fecfa33970eea810
Yonle <yonle@lecturify.net>
2024-01-28 22:49:07 +0700

    Need to set Cross-Origin-Resource-Policy also.
    Signed-off-by: Yonle <yonle@lecturify.net>

--- src/proxy.js
+++ src/proxy.js
@@ -58,6 +58,7 @@ function _onRequestResponse(origin, req, res) {
   copyHeaders(origin, res);
   res.setHeader("content-encoding", "identity");
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
   req.params.originType = origin.headers["content-type"] || "";
   req.params.originSize = origin.headers["content-length"] || "0";
 
