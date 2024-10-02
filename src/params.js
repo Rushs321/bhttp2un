60a94f12ad131127ba28111f18601016c0651584
Rushs321 <fyggh566@tutamail.com>
2024-10-02 15:48:48 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,16 +1,18 @@
 "use strict";
-const DEFAULT_QUALITY = 40
+const DEFAULT_QUALITY = 1;
 
 function params(req, res, next) {
-  let url = req.query.url;
-  if (!url) return res.send('bandwidth-hero-proxy');
+    let url = req.query.url;
+    if (Array.isArray(url)) url = url.join('&url=');
+    if (!url) return res.end('bandwidth-hero-proxy');
 
-  req.params.url = decodeURIComponent(url);
-  req.params.webp = !req.query.jpeg
-  req.params.grayscale = req.query.bw != 0
-  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
+    url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
+    req.params.url = url;
+    req.params.webp = !req.query.jpeg;
+    req.params.grayscale = req.query.bw != 0;
+    req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
 
-  next()
+    next()
 }
 
-module.exports = params
+module.exports = params;
